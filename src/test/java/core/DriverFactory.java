afdfe078599f7d307bc0e274fa5d8b7d629fc179
mpchandra <mpchandra.22@gmail.com>
2026-04-28 20:53:30 +0530

    Added CI pipeline

--- src/test/java/core/DriverFactory.java
+++ src/test/java/core/DriverFactory.java
@@ -3,6 +3,7 @@ package core;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 private static ThreadLocal<WebDriver> driver=new ThreadLocal<>();
